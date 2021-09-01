1a53211d57a34fda9bff869b0de72a3a2844d8b8
Sanjeev0609 <84101010+Sanjeev0609@users.noreply.github.com>
2021-09-01 23:30:10 +0530

    added registration page

---
+++ app/src/main/java/com/example/builderstool/RegisterFragment.kt
@@ -0,0 +1,24 @@
+package com.example.builderstool
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RegisterFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
+
+
